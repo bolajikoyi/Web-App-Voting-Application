e0c8c35c37613cb499a524951429fb49b8b77ea1
bolajikoyi <56487032+bolajikoyi@users.noreply.github.com>
2020-08-10 07:17:26 +0200

    Application created with sorting by number of votes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-reddit';
+  articles: Article[];
+  addArticle(title: HTMLInputElement, link: HTMLInputElement) {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
+    return false;
+  }
+  constructor() {
+    this.articles = [
+      new Article('Pump', 'http://pump.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Facebook', 'http://facebook.com', 1),
+      new Article('Twitter', 'http://twitter.com', 3),
+      new Article('Instagram', 'http://instagram.com', 2),
+      new Article('Google', 'http://google.com', 1),
+      new Article('Spotify', 'http://spotify.com', 3),
+    ];
+  }
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
